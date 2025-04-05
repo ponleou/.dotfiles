cf0745ad4f1ffcce891b89069ccdd98ec95499c1
Ponleou Keo Sok <ponleousk@gmail.com>
2025-04-05 13:35:40 +1100

    autocommit on change in /home/ponleou/.dotfiles/

--- commit-update.sh
+++ commit-update.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-inotifywait -q -m -r --exclude '\.git' -e CLOSE_WRITE --format="git add . && git commit -m 'autocommit on change in %w' && git push" ~/.dotfiles | sh
+inotifywait -q -m -r --exclude '\.git' \
+  -e CLOSE_WRITE \
+  -e CREATE \
+  -e DELETE \
+  -e MOVED_TO \
+  -e MOVED_FROM \
+  -e MODIFY \
+  -e ATTRIB \
+  -e UNLINK \
+  --format="git add . && git commit -m 'autocommit on change in %w' && git push" ~/.dotfiles | sh
