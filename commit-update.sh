8dc0298c4151b58e41b782174d497a2eff0800dc
Ponleou Keo Sok <ponleousk@gmail.com>
2025-04-05 13:29:41 +1100

    test

--- commit-update.sh
+++ commit-update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-inotifywait -q -m -r -e CLOSE_WRITE --format="git -C %w add . && git -C %w commit -m 'autocommit on change in %w' && git -C  push" ../.dotfiles | sh  test
+inotifywait -q -m -r -e CLOSE_WRITE --format="git -C %w add . && git -C %w commit -m 'autocommit on change in %w' && git -C  push" ../.dotfiles | sh 
+test
