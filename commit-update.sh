4f04203fe9525304e11273bfd5607f11847029be
Ponleou Keo Sok <ponleousk@gmail.com>
2025-04-05 13:15:23 +1100

    autocommit on change in ../.dotfiles/

--- commit-update.sh
+++ commit-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-inotifywait -q -m -r -e CLOSE_WRITE --format="git -C %w add . && git -C %w commit -m 'autocommit on change in %w' && git push" ~/.dotfiles | sh
+inotifywait -q -m -r -e CLOSE_WRITE --format="git -C %w add . && git -C %w commit -m 'autocommit on change in %w' && git -C  push" ../.dotfiles | sh
 
 testest
