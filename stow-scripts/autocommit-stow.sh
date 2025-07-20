d01fcbac0429aba8a8db7282b5ddba149047808c
Keo Ponleou Sok <ponleousk@gmail.com>
2025-07-20 20:21:10 +1000

    autocommit: change in /home/ponleou/.dotfiles/stow-scripts/

--- stow-scripts/autocommit-stow.sh
+++ stow-scripts/autocommit-stow.sh
@@ -6,7 +6,6 @@ SYNC_TIMER_PID=""
 trigger_sync_after_idle() {
     if [ -n "$SYNC_TIMER_PID" ]; then
       kill "$SYNC_TIMER_PID"
-      notify-send $SYNC_TIMER_PID
       SYNC_TIMER_PID="" 
 
     ( sleep 10 && bash $SCRIPT_DIR/autosync-stow.sh ) &
