96bcf1cd960c396a5df42bfef5ea90b1c86a60cb
Keo Ponleou Sok <ponleousk@gmail.com>
2025-07-20 20:33:59 +1000

    autosync: commiting previous merge (20-07-2025 20:33:59)

--- stow-scripts/autosync-stow.sh
+++ stow-scripts/autosync-stow.sh
@@ -4,7 +4,10 @@ set -e
 
 cd "$TMP_DIR"
 
-git fetch origin
+git add -A
+git commit -m "autosync: commiting previous merge ($(date +'%d-%m-%Y %H:%M:%S'))"
+
 notify-send "Autosync is merging" "$(git merge --squash -X theirs origin/autocommit)"
 git commit -m "autosync: sync from autocommit branch ($(date +'%d-%m-%Y %H:%M:%S'))"
+
 notify-send "Autosync is pushing" "$(git push)"
