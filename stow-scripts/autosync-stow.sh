14900e83b9ad257e8e3b9661da7b55b428a36c36
Keo Ponleou Sok <ponleousk@gmail.com>
2025-07-21 17:28:50 +1000

    autosync: sync from autocommit branch (21-07-2025 17:28:50)

--- stow-scripts/autosync-stow.sh
+++ stow-scripts/autosync-stow.sh
@@ -5,6 +5,7 @@ set -e
 cd "$TMP_DIR"
 
 notify-send "Autosync is merging" "$(git merge --squash -X theirs origin/autocommit)"
+git add -A
 git commit -m "autosync: sync from autocommit branch ($(date +'%d-%m-%Y %H:%M:%S'))"
 
 notify-send "Autosync is pushing" "$(git push)"
