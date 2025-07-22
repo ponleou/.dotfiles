134ee22cd71255ad2c340f114d25910a9fd47fa5
Keo Ponleou Sok <ponleousk@gmail.com>
2025-07-22 17:05:32 +1000

    autocommit: change in /home/ponleou/.dotfiles/stow-scripts/

--- stow-scripts/autosync-stow.sh
+++ stow-scripts/autosync-stow.sh
@@ -20,10 +20,10 @@ fi
 
 cd "$TMP_DIR"
 
-notify-send "Autosync is merging" "$(git merge --squash -X theirs origin/autocommit)"
-git add -A
-git commit -m "autosync: sync from autocommit branch ($(date +'%d-%m-%Y %H:%M:%S'))"
+# notify-send "Autosync is merging" "$(git merge --squash -X theirs origin/autocommit)"
+# git add -A
+# git commit -m "autosync: sync from autocommit branch ($(date +'%d-%m-%Y %H:%M:%S'))"
 
-notify-send "Autosync is pushing" "$(git push)"
+# notify-send "Autosync is pushing" "$(git push)"
 
-notify-send "Autosync completed"
+# notify-send "Autosync completed"
