c08e95394121b559c59aba2ba6244ddf29717c19
Keo Ponleou Sok <ponleousk@gmail.com>
2025-07-22 17:10:35 +1000

    autocommit: change in /home/ponleou/.dotfiles/stow-scripts/

--- stow-scripts/autosync-stow.sh
+++ stow-scripts/autosync-stow.sh
@@ -20,6 +20,12 @@ fi
 
 cd "$TMP_DIR"
 
+LOCK_FILE="autosync.lock"
+if [ -f "$LOCK_FILE" ]; then
+  notify-send "Autosync aborted" "$TMP_DIR/$LOCK_FILE exists"
+  exit 1
+fi
+
 # notify-send "Autosync is merging" "$(git merge --squash -X theirs origin/autocommit)"
 # git add -A
 # git commit -m "autosync: sync from autocommit branch ($(date +'%d-%m-%Y %H:%M:%S'))"
