5902c231a38f0b46c37638763b051eb34f0628cb
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-07 00:00:42 +1100

    autocommit: change in /home/ponleou/.dotfiles/

--- stow-mocha.sh
+++ stow-mocha.sh
@@ -10,7 +10,7 @@ for theme in "${primary_themes[@]}"; do
   fi
 done
 
-stow --dir=./mocha/base --target=$HOME btop dolphin konsole ghostwriter
+stow --dir=./mocha/base --target=$HOME btop konsole ghostwriter
 
 if [[ $valid_theme == 1 ]]; then
     stow --dir=./mocha/$1 --target=$HOME nwg-look qt6ct rofi sway swaylock swaync waybar wlogout
