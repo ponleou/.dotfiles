9266ebc861bd9a2a88b8d73db924707c830c1dc0
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-06 23:37:41 +1100

    autocommit: change in /home/ponleou/.dotfiles/

--- stow-mocha.sh
+++ stow-mocha.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-stow --dir=./mocha/base --target=$HOME btop dolphin konsole nwg-look qt6ct rofi sway swaylock swaync waybar wlogout cliphist ghostwriter
+stow --dir=./mocha/base --adopt --target=$HOME btop dolphin konsole nwg-look qt6ct rofi sway swaylock swaync waybar wlogout cliphist ghostwriter
 
