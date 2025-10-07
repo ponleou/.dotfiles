196a042182262047f4d08d74e7a87d66cbf939cb
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-07 17:42:20 +1100

    autocommit: change in /home/ponleou/.dotfiles/

--- stow-mocha.sh
+++ stow-mocha.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-primary_themes=("peach")
-valid_theme=0
+accents=("peach")
+valid_accent=0
 script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 
-for theme in "${primary_themes[@]}"; do
-  if [[ "$1" == "$theme" ]]; then
-    valid_theme=1
+for accent in "${accents[@]}"; do
+  if [[ "$1" == "$accent" ]]; then
+    valid_accent=1
     break
   fi
 done
@@ -14,10 +14,15 @@ done
 stow --dir=$script_dir/mocha/base --target=$HOME btop konsole ghostwriter nwg-look qt6ct swaylock rofi swaync waybar wlogout
 stow --dir=$script_dir/mocha/base --target=$script_dir/essentials/utils sway-util
 
-if [[ $valid_theme == 1 ]]; then
-  stow --dir=$script_dir/mocha/$1 --target=$HOME nwg-look qt6ct
-  stow --dir=$script_dir/mocha/$1 --target=$script_dir/mocha/utils rofi-util swaync-util waybar-util wlogout-util sway-util
+stow_theme() {
+  local accent="$1"   # this is the variable after --dir=$script_dir/mocha/
+
+  stow --dir="$script_dir/mocha/$accent" --target="$HOME" nwg-look qt6ct
+  stow --dir="$script_dir/mocha/$accent" --target="$script_dir/mocha/utils" rofi-util swaync-util waybar-util wlogout-util sway-util
+}
+
+if [[ $valid_accent == 1 ]]; then
+  stow_theme "$1"
 else
-  stow --dir=$script_dir/mocha/"${primary_themes[0]}" --target=$HOME nwg-look qt6ct
-  stow --dir=$script_dir/mocha/"${primary_themes[0]}" --target=$script_dir/mocha/utils rofi-util swaync-util waybar-util wlogout-util sway-util
+  stow_theme "${accents[0]}"
 fi
