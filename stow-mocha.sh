7b09cf27e35f4d266c1b2ad7f4fa1f0ba6b7596e
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-14 18:26:05 +1100

    autocommit: change in /home/ponleou/.dotfiles/

--- stow-mocha.sh
+++ stow-mocha.sh
@@ -36,9 +36,7 @@ stow_accent() {
 
   echo $accent > "$script_dir/.current_accent"
 
-  nwg-look -a > /dev/null 2>&1
   papirus-folders -C cat-mocha-$accent > /dev/null 2>&1
-  swaync-client --reload-css >/dev/null 2>&1
 }
 
 if [[ $valid_accent == 1 ]]; then
@@ -57,3 +55,7 @@ else
 fi
 
 swaymsg reload
+swaync-client --reload-css >/dev/null 2>&1
+  nwg-look -a > /dev/null 2>&1
+
+
