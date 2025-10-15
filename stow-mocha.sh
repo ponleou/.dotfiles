c7c98ec80a3a1578f0fe8d19a20705a91fc6d79f
Keo Ponleou Sok <ponleousk@gmail.com>
2025-10-15 23:43:02 +1100

    autosync: sync from autocommit branch (15-10-2025 23:43:02)

--- stow-mocha.sh
+++ stow-mocha.sh
@@ -20,7 +20,7 @@ script_dir="$(dirname "$(realpath "$0")")" # directory of where the script is
 
 
 stow --dir=$script_dir/mocha/base --target=$HOME btop konsole ghostwriter nwg-look qt6ct swaylock rofi swaync waybar wlogout
-stow --dir=$script_dir/mocha/base --target=$script_dir/essentials/bases sway-base
+stow --dir=$script_dir/mocha/base --target=$script_dir/essentials/bases sway-base vesktop-base
 
 stow_accent() {
   local accent="$1"   # this is the variable after --dir=$script_dir/mocha/
@@ -28,11 +28,11 @@ stow_accent() {
   if [[ -f "$script_dir/settings/.current_accent" ]]; then
     local prev_accent=$(cat "$script_dir/settings/.current_accent")
     stow -D --dir="$script_dir/mocha/$prev_accent" --target="$HOME" nwg-look qt6ct
-    stow -D --dir="$script_dir/mocha/$prev_accent" --target="$script_dir/mocha/options" rofi-option swaync-option waybar-option wlogout-option sway-option
+    stow -D --dir="$script_dir/mocha/$prev_accent" --target="$script_dir/mocha/options" rofi-option swaync-option waybar-option wlogout-option sway-option vesktop-option
   fi
 
   stow --dir="$script_dir/mocha/$accent" --target="$HOME" nwg-look qt6ct
-  stow --dir="$script_dir/mocha/$accent" --target="$script_dir/mocha/options" rofi-option swaync-option waybar-option wlogout-option sway-option
+  stow --dir="$script_dir/mocha/$accent" --target="$script_dir/mocha/options" rofi-option swaync-option waybar-option wlogout-option sway-option vesktop-option
 
   echo $accent > "$script_dir/settings/.current_accent"
 
@@ -44,19 +44,19 @@ stow_mods() {
     local prev_background=$(cat "$script_dir/settings/.current_mod_background")
     local prev_fx=$(cat "$script_dir/settings/.current_mod_fx")
 
-    stow -D --dir=$script_dir/mocha/configs/background --target=$script_dir/mocha/mods "$prev_background"
-    stow -D --dir=$script_dir/mocha/configs/fx --target=$script_dir/mocha/mods "$prev_fx"
+    stow -D --dir=$script_dir/mocha/modlist/background --target=$script_dir/mocha/mods "$prev_background"
+    stow -D --dir=$script_dir/mocha/modlist/fx --target=$script_dir/mocha/mods "$prev_fx"
   fi
 
   if [[ $enable_blur == 1 ]]; then
-    stow --dir=$script_dir/mocha/configs/background --target=$script_dir/mocha/mods transparent
-    stow --dir=$script_dir/mocha/configs/fx --target=$script_dir/mocha/mods blur
+    stow --dir=$script_dir/mocha/modlist/background --target=$script_dir/mocha/mods transparent
+    stow --dir=$script_dir/mocha/modlist/fx --target=$script_dir/mocha/mods blur
 
     echo transparent > "$script_dir/settings/.current_mod_background"
     echo blur > "$script_dir/settings/.current_mod_fx"
   else 
-    stow --dir=$script_dir/mocha/configs/background --target=$script_dir/mocha/mods default
-    stow --dir=$script_dir/mocha/configs/fx --target=$script_dir/mocha/mods default
+    stow --dir=$script_dir/mocha/modlist/background --target=$script_dir/mocha/mods default
+    stow --dir=$script_dir/mocha/modlist/fx --target=$script_dir/mocha/mods default
 
     echo default > "$script_dir/settings/.current_mod_background"
     echo default > "$script_dir/settings/.current_mod_fx"
